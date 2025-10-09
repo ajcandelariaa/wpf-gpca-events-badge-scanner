31a15f94ccaa3b396bb392c8b5a309c71a0ce6f3
Aj Candelaria <ajajcandelaria@gmail.com>
2025-10-09 14:32:38 +0400

    RCC added

--- GPCAEventsBadgeScanner/Model/EventModel.cs
+++ GPCAEventsBadgeScanner/Model/EventModel.cs
@@ -7,7 +7,7 @@ namespace GPCAEventsBadgeScanner.Model
         public static string EventCategory = ConfigurationManager.AppSettings["EventCategory"];
         public static string EventYear = ConfigurationManager.AppSettings["EventYear"];
         public static string ApiUrl = ConfigurationManager.AppSettings["ApiUrl"];
-        public static string EventBanner = "/GPCAEventsBadgeScanner;component/Assets/Images/Banners/img_2025_anc.jpg";
+        public static string EventBanner = "/GPCAEventsBadgeScanner;component/Assets/Images/Banners/img_2025_rcc.jpg";
         public static string APIEndpoint { get; set; }
 
         static EventModel()
